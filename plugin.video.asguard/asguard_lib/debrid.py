d07917429a11bc76e081b42daab92dd626ca17d4
MrBlamo <druggedstoner@gmail.com>
2024-07-04 17:32:56 -0600

    Add files via upload

--- plugin.video.asguard/asguard_lib/debrid.py
+++ plugin.video.asguard/asguard_lib/debrid.py
@@ -2,7 +2,7 @@
 
 """
     Asguard Add-on
-    Copyright (C) 2016 Thor
+    Copyright (C) 2024 MrBlamo  
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,18 +18,23 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
-import urllib,json,time
+import json
+import time
 import kodi
+import urllib.parse
+import six
+from six.moves import urllib_request, urllib_parse
+import log_utils
 
 import cache
 from asguard_lib import client
 
+logger = log_utils.Logger.get_logger()
 
 def rdAuthorize():
     try:
         CLIENT_ID = 'X245A4XAIBGVM'
-        USER_AGENT = 'Kodi Asguard/3.0'
+        USER_AGENT = 'Kodi Asguard/1.0'
 
         if not '' in credentials()['realdebrid'].values():
             if kodi.yesnoDialog(kodi.lang(32531).encode('utf-8'), kodi.lang(32532).encode('utf-8'), '', 'RealDebrid'):
@@ -70,7 +75,7 @@ def rdAuthorize():
         id, secret = result['client_id'], result['client_secret'] 
 
         url = 'https://api.real-debrid.com/oauth/v2/token'
-        post = urllib.urlencode({'client_id': id, 'client_secret': secret, 'code': device_code, 'grant_type': 'http://oauth.net/grant_type/device/1.0'})
+        post = urllib_parse.urlencode({'client_id': id, 'client_secret': secret, 'code': device_code, 'grant_type': 'http://oauth.net/grant_type/device/1.0'})
 
         result = client.request(url, post=post, headers=headers)
         result = json.loads(result)
@@ -86,7 +91,6 @@ def rdAuthorize():
     except:
         kodi.openSettings('3.16')
 
-
 def rdDict():
     try:
         if '' in credentials()['realdebrid'].values(): raise Exception()
@@ -98,75 +102,27 @@ def rdDict():
     except:
         return []
 
-
-def pzDict():
-    try:
-        if '' in credentials()['premiumize'].values(): raise Exception()
-        user, password = credentials()['premiumize']['user'], credentials()['premiumize']['pass']
-        url = 'http://api.premiumize.me/pm-api/v1.php?method=hosterlist&params[login]=%s&params[pass]=%s' % (user, password)
-        result = cache.get(client.request, 24, url)
-        hosts = json.loads(result)['result']['hosterlist']
-        hosts = [i.lower() for i in hosts]
-        return hosts
-    except:
-        return []
-
-
-def adDict():
-    try:
-        if '' in credentials()['alldebrid'].values(): raise Exception()
-        url = 'http://alldebrid.com/api.php?action=get_host'
-        result = cache.get(client.request, 24, url)
-        hosts = json.loads('[%s]' % result)
-        hosts = [i.lower() for i in hosts]
-        return hosts
-    except:
-        return []
-
-
-def rpDict():
-    try:
-        if '' in credentials()['rpnet'].values(): raise Exception()
-        url = 'http://premium.rpnet.biz/hoster2.json'
-        result = cache.get(client.request, 24, url)
-        result = json.loads(result)
-        hosts = result['supported']
-        hosts = [i.lower() for i in hosts]
-        return hosts
-    except:
-        return []
-
-
-def debridDict():
-    return {
-    'realdebrid': rdDict(),
-    'premiumize': pzDict(),
-    'alldebrid': adDict(),
-    'rpnet': rpDict()
-    }
-
-
 def credentials():
     return {
         'realdebrid': {
-        'id': kodi.setting('realdebrid.id'),
-        'secret': kodi.setting('realdebrid.secret'),
-        'token': kodi.setting('realdebrid.token'),
-        'refresh': kodi.setting('realdebrid.refresh')
-    },
+            'id': kodi.setting('realdebrid.id'),
+            'secret': kodi.setting('realdebrid.secret'),
+            'token': kodi.setting('realdebrid.token'),
+            'refresh': kodi.setting('realdebrid.refresh')
+        },
         'premiumize': {
-        'user': kodi.setting('premiumize.user'),
-        'pass': kodi.setting('premiumize.pin')
-    },
+            'user': kodi.setting('premiumize.user'),
+            'pass': kodi.setting('premiumize.pass')
+        },
         'alldebrid': {
-        'user': kodi.setting('alldebrid.user'),
-        'pass': kodi.setting('alldebrid.pass')
-    },
+            'user': kodi.get_setting('alldebrid.user'),
+            'pass': kodi.get_setting('alldebrid.pass')
+        },
         'rpnet': {
-        'user': kodi.setting('rpnet.user'),
-        'pass': kodi.setting('rpnet.api')
-    }}
-
+            'user': kodi.setting('rpnet.user'),
+            'pass': kodi.setting('rpnet.api')
+        }
+    }
 
 def status():
     try:
@@ -175,7 +131,6 @@ def status():
         else: return True
     except:
         return False
-
 
 def resolver(url, debrid):
     u = url
@@ -187,9 +142,9 @@ def resolver(url, debrid):
         if '' in credentials()['realdebrid'].values(): raise Exception()
         id, secret, token, refresh = credentials()['realdebrid']['id'], credentials()['realdebrid']['secret'], credentials()['realdebrid']['token'], credentials()['realdebrid']['refresh']
 
-        USER_AGENT = 'Kodi Asguard/3.0'
+        USER_AGENT = 'Kodi Asguard/1.0'
 
-        post = urllib.urlencode({'link': u})
+        post = urllib_parse.urlencode({'link': u})
         headers = {'Authorization': 'Bearer %s' % token, 'User-Agent': USER_AGENT}
         url = 'https://api.real-debrid.com/rest/1.0/unrestrict/link'
 
@@ -197,7 +152,7 @@ def resolver(url, debrid):
         result = json.loads(result)
 
         if 'error' in result and result['error'] == 'bad_token':
-            result = client.request('https://api.real-debrid.com/oauth/v2/token', post=urllib.urlencode({'client_id': id, 'client_secret': secret, 'code': refresh, 'grant_type': 'http://oauth.net/grant_type/device/1.0'}), headers={'User-Agent': USER_AGENT}, error=True)
+            result = client.request('https://api.real-debrid.com/oauth/v2/token', post=urllib_parse.urlencode({'client_id': id, 'client_secret': secret, 'code': refresh, 'grant_type': 'http://oauth.net/grant_type/device/1.0'}), headers={'User-Agent': USER_AGENT}, error=True)
             result = json.loads(result)
             if 'error' in result: return
 
@@ -216,7 +171,7 @@ def resolver(url, debrid):
         if '' in credentials()['premiumize'].values(): raise Exception()
         user, password = credentials()['premiumize']['user'], credentials()['premiumize']['pass']
 
-        url = 'http://api.premiumize.me/pm-api/v1.php?method=directdownloadlink&params[login]=%s&params[pass]=%s&params[link]=%s' % (user, password, urllib.quote_plus(u))
+        url = 'http://api.premiumize.me/pm-api/v1.php?method=directdownloadlink&params[login]=%s&params[pass]=%s&params[link]=%s' % (user, password, urllib_parse.quote_plus(u))
         result = client.request(url, close=False)
         url = json.loads(result)['result']['location']
         return url
@@ -229,15 +184,19 @@ def resolver(url, debrid):
         if '' in credentials()['alldebrid'].values(): raise Exception()
         user, password = credentials()['alldebrid']['user'], credentials()['alldebrid']['pass']
 
-        login_data = urllib.urlencode({'action': 'login', 'login_login': user, 'login_password': password})
+        login_data = urllib_parse.urlencode({'action': 'login', 'login_login': user, 'login_password': password})
         login_link = 'http://alldebrid.com/register/?%s' % login_data
         cookie = client.request(login_link, output='cookie', close=False)
 
-        url = 'http://www.alldebrid.com/service.php?link=%s' % urllib.quote_plus(u)
+        url = 'http://www.alldebrid.com/service.php?link=%s' % urllib_parse.quote_plus(u)
+        logger.log('alldebrid url: %s' % url, log_utils.LOGNOTICE)
         result = client.request(url, cookie=cookie, close=False)
-        url = client.parseDOM(result, 'a', ret='href', attrs = {'class': 'link_dl'})[0]
-        url = client.replaceHTMLCodes(url)
-        url = '%s|Cookie=%s' % (url, urllib.quote_plus(cookie))
+        logger.log('alldebrid result: %s' % result, log_utils.LOGNOTICE)
+        
+        # Parse the JSON response
+        result_json = json.loads(result)
+        url = result_json['link']
+        logger.log('alldebrid url: %s' % url, log_utils.LOGNOTICE)
         return url
     except:
         pass
@@ -248,7 +207,7 @@ def resolver(url, debrid):
         if '' in credentials()['rpnet'].values(): raise Exception()
         user, password = credentials()['rpnet']['user'], credentials()['rpnet']['pass']
 
-        login_data = urllib.urlencode({'username': user, 'password': password, 'action': 'generate', 'links': u})
+        login_data = urllib.parse.urlencode({'username': user, 'password': password, 'action': 'generate', 'links': u})
         login_link = 'http://premium.rpnet.biz/client_api.php?%s' % login_data
         result = client.request(login_link, close=False)
         result = json.loads(result)
@@ -257,4 +216,46 @@ def resolver(url, debrid):
     except:
         return
 
+def adDict():
+    try:
+        if '' in credentials()['alldebrid'].values(): raise Exception()
+        url = 'https://api.alldebrid.com/v4/hosts?agent=Asguard'
+        logger.log('adDict url: %s' % url, log_utils.LOGNOTICE)
+        result = cache.get(client.request, 24, url)
+        hosts = json.loads(result)['hosts']
+        hosts = [i.lower() for i in hosts]
+        return hosts
+    except:
+        return []
 
+def debridDict():
+    return {
+        'realdebrid': rdDict(),
+        'premiumize': pzDict(),
+        'alldebrid': adDict(),
+        'rpnet': rpDict()
+    }
+
+def pzDict():
+    try:
+        if '' in credentials()['premiumize'].values(): raise Exception()
+        user, password = credentials()['premiumize']['user'], credentials()['premiumize']['pass']
+        url = 'http://api.premiumize.me/pm-api/v1.php?method=hosterlist&params[login]=%s&params[pass]=%s' % (user, password)
+        result = cache.get(client.request, 24, url)
+        hosts = json.loads(result)['result']['hosterlist']
+        hosts = [i.lower() for i in hosts]
+        return hosts
+    except:
+        return []
+
+def rpDict():
+    try:
+        if '' in credentials()['rpnet'].values(): raise Exception()
+        url = 'http://premium.rpnet.biz/hoster2.json'
+        result = cache.get(client.request, 24, url)
+        result = json.loads(result)
+        hosts = result['supported']
+        hosts = [i.lower() for i in hosts]
+        return hosts
+    except:
+        return []
